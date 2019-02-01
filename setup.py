a1d617cdbc3fb42d228fb003c511a394b2d97a16
XiaoboSun <sunxb05@gmail.com>
2019-02-01 11:04:12 +0100

    add setup

---
+++ setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(
+    name='qmworks',
+    version='0.3.0',
+    description='Automation of computations in quantum chemistry',
+    license='Apache 2.0',
+    url='https://github.com/sunxb05/PyFrag',
+    author=["Xiaobo Sun'],
+    author_email='f.zapata@esciencecenter.nl',
+    keywords='chemistry workflows simulation materials',
+    long_description=readme(),
+    package_dir={'': 'qmworks'},
+    packages=["qmflows",
+              "qmflows.components",
+              "qmflows.data",
+              "qmflows.data.dictionaries",
+              "qmflows.data.coskf",
+              "qmflows.examples",
+              "qmflows.examples.Conditional_workflows",
+              "qmflows.examples.Constrained_and_TS_optimizations",
+              "qmflows.examples.FDE_Fragments",
+              "qmflows.hdf5",
+              "qmflows.packages",
+              "qmflows.parsers",
+              "qmflows.templates"],
+    package_data={
+        "qmflows": ['data/templates/*json', 'data/dictionaries/*json', 'data/coskf/*coskf']
+    },
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3.6',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Chemistry'
+    ],
+    install_requires=['h5py', 'numpy', 'noodles==0.3.1', 'plams>=1.2', 'pymonad',
+                      'pyparsing', 'filelock', 'openpyxl', 'pyyaml', 'xlrd', 'scipy'],
+    dependency_links=[
+        "git+https://github.com/SCM-NV/PLAMS@master#egg=plams-1.2"],
+
+    extras_require={
+        'test': ['pytest', 'pytest-cov', 'pytest-mock', 'nbsphinx', 'pygraphviz'],
+        'doc': ['sphinx', 'sphinx_rtd_theme', 'nbsphinx']
+    }
