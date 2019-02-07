affe45dc9cf16934ef6aa9bbbf1ecb4c3748539d
XiaoboSun <sunxb05@gmail.com>
2019-02-07 01:04:22 +0100

    doc

--- docs/conf.py
+++ docs/conf.py
@@ -15,13 +15,13 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-# import os
-# import subprocess
+import os
+import subprocess
 
-# on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-# if on_rtd:
-#     subprocess.call('doxygen', shell=True)
+if on_rtd:
+    subprocess.call('doxygen', shell=True)
 
 
 extensions = [
