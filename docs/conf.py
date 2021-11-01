6363513d756bdfa54d99762689b119a66b5e90cb
xiaobo <xiaobosun@Xiaobos-MacBook-Pro.local>
2021-11-01 19:18:31 +0100

    doc test

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = '2.2'
+needs_sphinx = '2.1'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
