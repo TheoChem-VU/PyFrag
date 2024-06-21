671e2c340b1d8a56fc1d6160532dbe593155b833
SiebeLeDe <s.j.lekanne.deprez@vu.nl>
2024-06-21 14:22:20 +0200

    Redone the sbatch option to prevent unexpected submissions to the login node

--- host/bin/open.sh
+++ host/bin/open.sh
@@ -15,10 +15,10 @@ bash "$HOSTPYFRAG"/standalone/adf_open/pyfragparce.sh "$1"
 
 # The user may run this file on a cluster (sbatch) or a local machine (bash).
 # However, the user must have either sbatch or bash installed (which may not be the case on Windows computers).
-if command -v sbatch >/dev/null 2>&1; then
-    sbatch "$JOBDIR/sub"
-elif command -v bash >/dev/null 2>&1; then
-    bash "$JOBDIR/sub"
-else
-    echo "Warning: Please activate a shell that supports sbatch or bash."
-fi
+# if command -v sbatch >/dev/null 2>&1; then
+sbatch "$JOBDIR/sub"
+# elif command -v bash >/dev/null 2>&1; then
+#     bash "$JOBDIR/sub"
+# else
+#     echo "Warning: Please activate a shell that supports sbatch or bash."
+# fi
