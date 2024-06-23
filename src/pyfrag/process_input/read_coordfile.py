57e5ce2b7e3c09fc5cb77a436cbdd69a5f09d3f7
SiebeLeDe <s.j.lekanne.deprez@vu.nl>
2024-06-23 22:36:18 +0200

    coord file addition

--- src/pyfrag/process_input/read_coordfile.py
+++ src/pyfrag/process_input/read_coordfile.py
@@ -1,36 +1,130 @@
 from pathlib import Path
-from typing import Callable, Dict
+from typing import Callable, Dict, List, Union
 
-from scm.plams import Molecule
+import numpy as np
+from numpy.typing import NDArray
+from scm.plams import Atom, KFHistory, KFReader, Molecule, PeriodicTable, Units
 
 import pyfrag
 
+# =============================================================================
+# AMV / XYZ file reading  =====================================================
+# =============================================================================
 
-def _create_molecule_from_xyz(mol: Molecule, xyz_file: Path):
+
+def _create_molecule_from_xyz(xyz_file: Path) -> List[Molecule]:
     """
     Read a molecule or multiple molecules from a .xyz file.
     """
-    mol.read(xyz_file, "xyz")  # type: ignore  # plams does not have static typing
+    mols = []
+
+    current_molecule = Molecule()
+
+    lines = xyz_file.read_text().splitlines()
+
+    for line in lines:
+        line = line.strip()
+        if not line:  # Blank line indicates separation between molecules
+            if current_molecule.atoms:  # Check if the molecule has any atoms
+                mols.append(current_molecule)
+                current_molecule = Molecule()  # Start a new molecule
+        else:
+            # Assuming the line format is "Symbol X Y Z"
+            parts = line.split()
+
+            if parts[0] not in PeriodicTable.symtonum:  # Check if the first part is a valid element symbol
+                continue
+
+            if len(parts) == 4:  # Check if the line has 4 parts
+                symbol, x, y, z = parts
+                atom = Atom(symbol=symbol, coords=(float(x), float(y), float(z)))
+                current_molecule.add_atom(atom)
+
+    # After reading the file, check if the last molecule has atoms and hasn't been added yet
+    if current_molecule.atoms:
+        mols.append(current_molecule)
+
+    return mols
+
+
+def _create_molecule_from_amv(amv_file: Path) -> List[Molecule]:
+    """
+    Read a molecule or multiple molecules from an .amv file. Structure is very similar to .xyz file, but with an additional header line for each molecule.
+    """
+    mols = _create_molecule_from_xyz(amv_file)
+    return mols
+
+
+# =============================================================================
+# RKF file reading PESScans and IRCs ==========================================
+# =============================================================================
+
+
+def _create_molecule_from_coords_and_symbols(atom_symbols: List[str], atom_coords: Union[List[List[float]], NDArray]) -> Molecule:
+    """
+    Create a PLAMS molecule from atom symbols and coordinates.
+    """
+    mol = Molecule()
+    for atom_symbol, atom_coord in zip(atom_symbols, atom_coords):
+        mol.add_atom(Atom(symbol=atom_symbol, coords=atom_coord))
     return mol
 
 
-def _create_molecule_from_ams_rkf(mol: Molecule, rkf_file: Path):
+def _extract_molecules_from_pes_scan(history: KFHistory, atom_symbols: List[str], converged_indices: List[float]) -> List[Molecule]:
+    """Extract the molecules from a PES scan calculation"""
+    mols: List[Molecule] = []
+
+    for i, coord in enumerate(history.iter("Coords"), start=1):  # type: ignore  # Results does not have proper typing
+        if i not in converged_indices:
+            continue
+
+        coord = np.array(coord, dtype=float).reshape(-1, 3) * Units.conversion_ratio("bohr", "angstrom")
+
+        mols.append(_create_molecule_from_coords_and_symbols(atom_symbols, coord))
+
+    return mols
+
+
+def _extract_molecules_from_irc_path(history: KFHistory, atom_symbols: List[str]) -> List[Molecule]:
+    """Extract the molecules from an IRC calculation"""
+    mols_forward: List[Molecule] = []
+    mols_backward: List[Molecule] = []
+
+    for coord, direction, is_converged in zip(history.iter("Coords"), history.iter("IRCDirection"), history.iter("Converged")):  # type: ignore  # Results does not have proper typing
+        if not is_converged:
+            continue
+
+        coord = np.array(coord, dtype=float).reshape(-1, 3) * Units.conversion_ratio("bohr", "angstrom")
+        mol = _create_molecule_from_coords_and_symbols(atom_symbols, coord)
+        if direction == 1:  # Forward
+            mols_forward.append(mol)
+        elif direction == 2:  # Backward
+            mols_backward.append(mol)
+        else:
+            raise ValueError(f"Unknown IRC direction: {direction}")
+    return mols_backward[::-1] + mols_forward
+
+
+def _create_molecule_from_ams_rkf(rkf_file: Path) -> List[Molecule]:
     """
     Read a molecule or multiple molecules from an AMS .rkf file.
     """
-    mol = mol.read(rkf_file, "rkf")  # type: ignore  # plams does not have static typing
-    return mol
+    kf_reader = KFReader(rkf_file)
+    history_reader = KFHistory(kf_reader, "History")
+    atom_symbols: List[str] = kf_reader.read("Molecule", "AtomSymbols").split()  # type: ignore  # Results does not have proper typing
+
+    if ("IRC", "direction") in kf_reader:
+        mols = _extract_molecules_from_irc_path(history_reader, atom_symbols)
+    elif ("PESScan", "nPoints") in kf_reader:
+        converged_points = [index for index in kf_reader.read("PESScan", "HistoryIndices")]  # type: ignore  # Results does not have proper typing
+        mols = _extract_molecules_from_pes_scan(history_reader, atom_symbols, converged_points)
+    else:
+        raise ValueError(f"Unsupported .rkf file: {rkf_file}")
+
+    return mols
 
 
-def _create_molecule_from_amv(mol: Molecule, amv_file: Path):
-    """
-    Read a molecule or multiple molecules from an .amv file
-    """
-    mol = mol.read(amv_file, "xyz")  # type: ignore  # plams does not have static typing
-    return mol
-
-
-extension_func_mapping: Dict[str, Callable[[Molecule, Path], Molecule]] = {
+extension_func_mapping: Dict[str, Callable[[Path], List[Molecule]]] = {
     "xyz": _create_molecule_from_xyz,
     "amv": _create_molecule_from_amv,
     "rkf": _create_molecule_from_ams_rkf,
@@ -39,15 +133,14 @@ extension_func_mapping: Dict[str, Callable[[Molecule, Path], Molecule]] = {
 
 def main():
     coord_file_dir = Path(pyfrag.__file__).parent.parent.parent.resolve() / "tests" / "fixtures" / "coord_files"
-    print(coord_file_dir)
 
     extensions = ["xyz", "rkf", "amv"]
     files = {ext: coord_file_dir / f"ams.{ext}" for ext in extensions}
 
     for ext, file in files.items():
         mol = Molecule()
-        mol = extension_func_mapping[ext](mol, file)
-        print(ext, mol)
+        mol = extension_func_mapping[ext](file)
+        print(ext, len(mol), mol[31])
 
 
 if __name__ == "__main__":
--- tests/test_read_coordfile.py
+++ tests/test_read_coordfile.py
@@ -1,35 +1,70 @@
 from pathlib import Path
 
+import numpy as np
 import pytest
-from pyfrag.process_input.read_coordfile import _create_molecule_from_ams_rkf, _create_molecule_from_amv, _create_molecule_from_xyz
+from pyfrag.process_input.read_coordfile import _create_molecule_from_ams_rkf, _create_molecule_from_xyz
 from scm.plams import Molecule
 
 
-@pytest.fixture(params=["xyz", "amv", "rkf"])
-def coord_file_path(request):
+@pytest.fixture()
+def pes_rkf_file_path():
     current_file_path = Path(__file__).parent.resolve()
-    return current_file_path / "fixtures" / "coord_files" / f"ams.{request.param}"
+    return current_file_path / "fixtures" / "coord_files" / "pes.ams.rkf"
 
 
-def test_get_molecule_from_xyz(coord_file_path):
-    if coord_file_path.suffix == ".xyz":
-        mol = Molecule()
-        result = _create_molecule_from_xyz(mol, coord_file_path)
-        # Replace the following line with actual validation for your Molecule object
-        assert result is not None, "Failed to read .xyz file"
+@pytest.fixture()
+def xyz_file_path():
+    current_file_path = Path(__file__).parent.resolve()
+    return current_file_path / "fixtures" / "coord_files" / "ams.xyz"
 
 
-def test_create_molecule_from_ams_rkf(coord_file_path):
-    if coord_file_path.suffix == ".rkf":
-        mol = Molecule()
-        result = _create_molecule_from_ams_rkf(mol, coord_file_path)
-        # Replace the following line with actual validation for your Molecule object
-        assert result is not None, "Failed to read .rkf file"
+@pytest.fixture()
+def amv_file_path():
+    current_file_path = Path(__file__).parent.resolve()
+    return current_file_path / "fixtures" / "coord_files" / "ams.amv"
 
 
-def test_create_molecule_from_amv(coord_file_path):
-    if coord_file_path.suffix == ".amv":
-        mol = Molecule()
-        result = _create_molecule_from_amv(mol, coord_file_path)
-        # Replace the following line with actual validation for your Molecule object
-        assert result is not None, "Failed to read .rkf file"
+@pytest.fixture()
+def irc_rkf_file_path():
+    current_file_path = Path(__file__).parent.resolve()
+    return current_file_path / "fixtures" / "coord_files" / "irc.ams.rkf"
+
+
+def test_get_molecule_from_pes_rkf(pes_rkf_file_path):
+    pes_mols = _create_molecule_from_ams_rkf(pes_rkf_file_path)
+    assert len(pes_mols) == 15
+    assert isinstance(pes_mols[0], Molecule)
+
+
+def test_get_molecule_from_xyz(xyz_file_path):
+    mols = _create_molecule_from_xyz(xyz_file_path)
+    assert len(mols) == 33
+    assert isinstance(mols[0], Molecule)
+
+
+def test_get_molecule_from_amv(amv_file_path):
+    mols = _create_molecule_from_xyz(amv_file_path)
+    assert len(mols) == 33
+    assert isinstance(mols[0], Molecule)
+
+
+def test_get_molecule_from_irc_rkf(irc_rkf_file_path):
+    irc_mols = _create_molecule_from_ams_rkf(irc_rkf_file_path)
+    assert len(irc_mols) == 33
+    assert isinstance(irc_mols[0], Molecule)
+
+
+def test_same_molecule_from_irc_xyz_amv(irc_rkf_file_path, xyz_file_path, amv_file_path):
+    irc_mols = _create_molecule_from_ams_rkf(irc_rkf_file_path)
+    xyz_mols = _create_molecule_from_xyz(xyz_file_path)
+    amv_mols = _create_molecule_from_xyz(amv_file_path)
+
+    assert len(irc_mols) == len(xyz_mols) == len(amv_mols), "Lengths are not equal"
+
+    assert np.allclose(irc_mols[0].as_array(), xyz_mols[0].as_array(), rtol=1e-5, atol=1e-8), "Arrays are not close enough"
+    assert np.allclose(irc_mols[0].as_array(), amv_mols[0].as_array(), rtol=1e-5, atol=1e-8), "Arrays are not close enough"
+    assert np.allclose(xyz_mols[0].as_array(), amv_mols[0].as_array(), rtol=1e-5, atol=1e-8), "Arrays are not close enough"
+
+    assert np.allclose(irc_mols[31].as_array(), xyz_mols[31].as_array(), rtol=1e-5, atol=1e-8), "Arrays are not close enough"
+    assert np.allclose(irc_mols[31].as_array(), amv_mols[31].as_array(), rtol=1e-5, atol=1e-8), "Arrays are not close enough"
+    assert np.allclose(xyz_mols[31].as_array(), amv_mols[31].as_array(), rtol=1e-5, atol=1e-8), "Arrays are not close enough"
