d17da7c6dc566d6344fe223fd4359ad5aca5c27f
SiebeLeDe <s.j.lekanne.deprez@vu.nl>
2024-07-05 20:41:01 +0200

    Added function to get a dictionary of input blocks + keys

--- src/pyfrag/read_input/inputblocks.py
+++ src/pyfrag/read_input/inputblocks.py
@@ -1,8 +1,9 @@
 import logging
 from dataclasses import asdict, dataclass
 from pprint import pprint
-from typing import Union
+from typing import Dict, Literal, Union
 
+from pyfrag.config.config import pyfrag_config
 from pyfrag.errors import PyFragSectionInputError
 
 logger = logging.getLogger(name="PyFrag Section Reader")
@@ -18,7 +19,21 @@ class InputBlocks:
     AMS: Union[str, None] = None
     FRAGMENT1_EXTRA: Union[str, None] = None
     FRAGMENT2_EXTRA: Union[str, None] = None
+    FRAGMENT1_OPEN_EXTRA: Union[str, None] = None
+    FRAGMENT2_OPEN_EXTRA: Union[str, None] = None
     COMPLEX_EXTRA: Union[str, None] = None
+
+    def get_input_blocks_specific_section(self, section_name: Literal["AMS", "PYFRAG", "JOBSUB"]) -> Dict[str, Union[str, None]]:
+        """Returns the content of the specific section."""
+        section_name_str = section_name.upper()
+
+        if section_name_str not in self.__dict__.keys():
+            raise PyFragSectionInputError(f"Section {section_name} is not present in the input file.")
+        if section_name_str == "AMS":
+            ams_keys = pyfrag_config.ams.input_blocks
+            return {k: v for k, v in self.__dict__.items() if k in ams_keys}
+        else:
+            return self.__dict__[section_name.upper()]
 
     def validate(self):
         if self.PYFRAG is None:
@@ -35,3 +50,6 @@ class InputBlocks:
 
     def __str__(self) -> str:
         return str(pprint(asdict(self)))
+
+    def __contains__(self, key: str):
+        return key in self.__dict__.keys()
