414053f0752808d1364d859eb27afbc38fa98c11
SiebeLeDe <s.j.lekanne.deprez@vu.nl>
2024-10-06 22:03:58 +0200

    Fragment extra and fragment open extra are now dictionaries instead of lists which enables the ability to assign a key to the fragment index (so 1: "fragment1 content", 2: "fragment2 content").

--- src/pyfrag/read_input/inputblocks.py
+++ src/pyfrag/read_input/inputblocks.py
@@ -1,9 +1,8 @@
 import logging
 import pprint
 from dataclasses import asdict, dataclass, field
-from typing import Dict, List, Literal, Union
+from typing import Dict, Literal, Union
 
-from pyfrag.config.config import pyfrag_config
 from pyfrag.errors import PyFragSectionInputError
 
 logger = logging.getLogger(name="PyFrag Section Reader")
@@ -17,8 +16,8 @@ class InputBlocks:
     PYFRAG: str = ""
     ADF: str = ""
     AMS: str = ""
-    FRAGMENT_EXTRA: List[str] = field(default_factory=list)
-    FRAGMENT_OPEN_EXTRA: List[str] = field(default_factory=list)
+    FRAGMENT_EXTRA: Dict[int, str] = field(default_factory=dict)
+    FRAGMENT_OPEN_EXTRA: Dict[int, str] = field(default_factory=dict)
     COMPLEX_EXTRA: str = ""
 
     def get_input_blocks_specific_section(self, section_name: Literal["SCM", "PYFRAG", "JOBSUB"]) -> Dict[str, Union[str, None]]:
@@ -29,7 +28,7 @@ class InputBlocks:
             raise PyFragSectionInputError(f"Section {section_name} is not present in the input file.")
 
         if section_name_str == "SCM":
-            ams_keys = pyfrag_config.ams.input_blocks
+            ams_keys = ["AMS", "ADF"]
             return {k: v for k, v in self.__dict__.items() if k in ams_keys}
         else:
             return self.__dict__[section_name.upper()]
@@ -41,13 +40,16 @@ class InputBlocks:
         if not self.ADF and not self.AMS:
             raise PyFragSectionInputError("ADF and AMS sections are missing in the input file.\nPlease make sure to include 'ADF' ... ''ADF END' or 'AMS' ... ''AMS END' in the input file.")
 
+        if self.ADF and self.AMS:
+            raise PyFragSectionInputError("Both ADF and AMS sections are present in the input file.\nPlease only include one of the two sections, preferably the AMS section.")
+
     def __iter__(self):
         for k, v in self.__dict__.items():
             if k == "FRAGMENT_EXTRA":
-                for i, fragment in enumerate(v, start=1):
+                for i, fragment in v.items():
                     yield f"FRAGMENT{i}_EXTRA", fragment
             elif k == "FRAGMENT_OPEN_EXTRA":
-                for i, fragment in enumerate(v, start=1):
+                for i, fragment in v.items():
                     yield f"FRAGMENT{i}_OPEN_EXTRA", fragment
             elif v:
                 yield k, v
