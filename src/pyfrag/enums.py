0a6452414224aaef320b9f5af805118d8703d7fb
SiebeLeDe <s.j.lekanne.deprez@vu.nl>
2024-10-06 21:59:03 +0200

    added jobstatus

--- src/pyfrag/enums.py
+++ src/pyfrag/enums.py
@@ -1,15 +1,28 @@
-from enum import Enum
+from enum import Enum, auto
 
 
 class JobStatus(Enum):
     """Enum for job status"""
 
-    INITIALIZED = 0
-    RUNNING = 1
-    FINISHED = 2
-    FAILED = 3
-    CANCELLED = 4
-    UNKNOWN = 5
+    INITIALIZED = auto()
+    PREPARED = auto()
+    RUNNING = auto()
+    FINISHED = auto()
+    FAILED = auto()
+    CANCELLED = auto()
+    UNKNOWN = auto()
 
     def __str__(self):
         return self.name.lower()
+
+
+class CalcType(Enum):
+    RESTRICTED = 1
+    UNRESTRICTED_SPINPOL = 2
+    UNRESTRICTED_NO_SPINPOL = 3
+
+
+class CalcProgram(Enum):
+    AMS = 1
+    ORCA = 2
+    GAUSSIAN = 3
