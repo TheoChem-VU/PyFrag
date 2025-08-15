def7cfa3bb7a5e6b2702c32a04ccf276626bb519
SiebeLeDe <siebbe@hotmail.com>
2025-08-15 13:02:11 +0200

    Added cluster-related deployment files

---
+++ deploy_on_cluster.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# ==================================================================
+# This script is used to deploy the PyFrag application on a High performance computer (HPC) cluster.
+# The following is required for deploying the PyFrag application:
+# - Install the Python package manager "uv" if it is not already installed
+# - Create a virtual environment (Python) with the pyfrag package installed through `uv sync`
+# ==================================================================
