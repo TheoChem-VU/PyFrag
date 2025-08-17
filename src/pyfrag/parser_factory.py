b2f9402eb9fde93817d2a75f74a0882b4204aa44
SiebeLeDe <siebbe@hotmail.com>
2025-08-17 11:57:07 +0200

    Simplified parser since the different executables have been rewritten to facilitate the same input format.

--- src/pyfrag/parser_factory.py
+++ src/pyfrag/parser_factory.py
@@ -6,15 +6,19 @@ import pyfrag.executables.adf.adf_parser as adf_parser
 
 class ExecutableType(StrEnum):
     ADF = "adf"
+    ORCA = "orca"
+    GAUSSIAN = "gaussian"
+    TURBOMOLE = "turbomole"
 
 
 ParserFunc = Callable[[str], Dict[str, str]]
 
 
-def get_parser_from_executable(executable: str) -> ParserFunc:
-    """Get the parser for a specific PyFrag executable."""
+def get_parser() -> ParserFunc:
+    """
+        Get the parser for a specific PyFrag executable.
+        Currently, all the executables use the same parser. It may be extended in the future.
 
-    if executable.lower() == ExecutableType.ADF:
-        return adf_parser.extract_sections
-    else:
-        raise ValueError(f"Unsupported executable: {executable}. Supported: {list(ExecutableType)}.")
+    The `adf_parser.extract_sections` function is used by the `main.py` script to extract the `jobsub` section that specifies the job submission parameters (to a job scheduler such as SLURM or PBS).
+    """
+    return adf_parser.extract_sections
