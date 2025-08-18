16c57acf7d1ebd73e1f657b709e09461c04dee71
SiebeLeDe <siebbe@hotmail.com>
2025-08-18 10:34:00 +0200

    Clarify comments in deploy_on_cluster.sh regarding cache usage and enhance final instructions for setting PYFRAGSOURCE environment variable

--- deploy_on_cluster.sh
+++ deploy_on_cluster.sh
@@ -37,7 +37,7 @@ else
 fi
 
 # 2. Create a virtual environment (Python) with the pyfrag package installed through `uv sync`
-# Do use cache (as this may give unexpected errors when solving dependencies)
+# Do not use cache (as this may give unexpected errors when solving dependencies)
 # Do not use developer dependencies as we don't want to include dependencies for testing (pytest) or documentation (sphinx)
 echo "Creating virtual environment with dependencies and installing the python package..."
 uv sync --no-cache --no-dev
@@ -51,10 +51,11 @@ else
 fi
 
 # 3. Uninstalling uv
+# See https://docs.astral.sh/uv/getting-started/installation/#uninstallation (accessed August 18 2025)
 echo "Uninstalling uv..."
 uv cache clean
 rm -r "$(uv python dir)"
 rm -r "$(uv tool dir)"
 rm ~/.local/bin/uv ~/.local/bin/uvx
 
-echo "Finally, please make sure to set the PYFRAGSOURCE environment variable in the module file to /src/pyfrag folder"
+echo "Finally, please make sure to set the PYFRAGSOURCE environment variable in the module file to /src/pyfrag folder so that the `pyfrag` shell script is accessible via the terminal."
